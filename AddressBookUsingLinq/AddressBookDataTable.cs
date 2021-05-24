937c668295cbb839b134446051057ae7a69c28ee
vundelayamini <vundelayamini@gmail.com>
2021-05-24 19:49:09 +0530

    [yamini]Add: Ability to add attributes to data table

--- AddressBookUsingLinq/AddressBookDataTable.cs
+++ AddressBookUsingLinq/AddressBookDataTable.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Text;
+using System.Linq;
+
 
 namespace AddressBookUsingLinq
 {
     class AddressBookDataTable
     {
         DataTable addressBookDataTable = new DataTable();
+        //Add Columns to DataTable
+            addressBookTable.Columns.Add("FirstName", typeof(string));
+            addressBookTable.Columns.Add("LastName", typeof(string));
+            addressBookTable.Columns.Add("Address", typeof(string));
+            addressBookTable.Columns.Add("City", typeof(string));
+            addressBookTable.Columns.Add("State", typeof(string));
+            addressBookTable.Columns.Add("Zip", typeof(int));
+            addressBookTable.Columns.Add("PhoneNumber", typeof(long));
+            addressBookTable.Columns.Add("Email", typeof(string));
     }
 
 }
