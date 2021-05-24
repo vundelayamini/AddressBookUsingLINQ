7614b13cf6c9f726ac47d421c4be1de3e7486070
vundelayamini <vundelayamini@gmail.com>
2021-05-24 19:24:24 +0530

    [yamini]Add: Welcome message to AddressBook Using LINQ

---
+++ AddressBookUsingLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookUsingLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to AddressBook Using Linq");
+        }
+    }
+}
